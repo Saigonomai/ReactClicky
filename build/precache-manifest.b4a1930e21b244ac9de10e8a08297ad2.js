839e403ceef8091d664cf5e040ff0ae886ae52ee
Chentao Tang <chentao.tang@mail.utoronto.ca>
2019-06-11 21:24:39 -0400

    updating gh-page with build folder

---
+++ build/precache-manifest.b4a1930e21b244ac9de10e8a08297ad2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41b329c6389198a106e42ff2e4a5fd40",
+    "url": "/Saigonomai/ReactClicky/index.html"
+  },
+  {
+    "revision": "fcb6070b4fb1eec1b445",
+    "url": "/Saigonomai/ReactClicky/static/css/main.bbe1b29f.chunk.css"
+  },
+  {
+    "revision": "51caee9dfb2aec0ae94c",
+    "url": "/Saigonomai/ReactClicky/static/js/2.259a31eb.chunk.js"
+  },
+  {
+    "revision": "fcb6070b4fb1eec1b445",
+    "url": "/Saigonomai/ReactClicky/static/js/main.2468fc88.chunk.js"
+  },
+  {
+    "revision": "3d1c0c063cd59222714e",
+    "url": "/Saigonomai/ReactClicky/static/js/runtime~main.d4072845.js"
+  }
+]);
